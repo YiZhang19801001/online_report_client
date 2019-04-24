b3c19a26424adbdd18422ea1334f683832fec8cf
yizhang199 <yizhang19801001@gmail.com>
2019-04-24 12:33:06 +1000

    done: style login form

--- src/app/auth/Login.jsx
+++ src/app/auth/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 export default () => {
+  const labels = { app_name: "online report" }; //!fake data
   const [formValues, setFormValues] = useState({
     email: "",
     password: "",
@@ -13,6 +14,9 @@ export default () => {
 
   const handleFormSubmit = e => {
     e.preventDefault();
+    if (formValues.email === "" || formValues.password === "") {
+      return false;
+    }
     axios.post(`http://localhost:8000/api/auth/login`, formValues).then(res => {
       localStorage.setItem(
         "aupos_online_report_user",
@@ -25,8 +29,12 @@ export default () => {
   };
   return (
     <div className="component-login">
+      <div className="header">{labels.app_name}</div>
       <form onSubmit={handleFormSubmit}>
         <div className="form-field">
+          <label htmlFor="email" className="text-label">
+            email
+          </label>
           <input
             type="text"
             placeholder="enter email"
@@ -36,6 +44,9 @@ export default () => {
           />
         </div>
         <div className="form-field">
+          <label htmlFor="password" className="text-label">
+            password
+          </label>
           <input
             type="password"
             placeholder="enter password"
@@ -44,12 +55,16 @@ export default () => {
             onChange={handleInputChange}
           />
         </div>
-        <div className="form-field">
+        <div className="form-field checkbox">
           <input
+            name="remember_me"
             type="checkbox"
             checked={formValues.remember_me}
             onChange={handleChecked}
           />
+          <label htmlFor="remember_me" className="checkbox-label">
+            remember_me
+          </label>
         </div>
         <div className="button-container">
           <button>confirm</button>
