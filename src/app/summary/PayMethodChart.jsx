1bbc4f8d54db57d6a54ac7c35833fb0ed4dd64ba
yizhang199 <yizhang19801001@gmail.com>
2019-04-24 10:22:47 +1000

    fixed: payment method chart background color

--- src/app/summary/PayMethodChart.jsx
+++ src/app/summary/PayMethodChart.jsx
@@ -19,13 +19,23 @@ export default ({ list }) => {
   });
 
   const backgroundColor = list.map(item => {
-    switch (item.paymenttype) {
+    switch (item.paymenttype.toLowerCase()) {
       case "wechat":
         return `#56B849`;
       case "cash":
         return `#ffba2d`;
-      case "alipay":
+      case "eftpos offline":
         return `#00AAEE`;
+      case "redpayments":
+        return `#E50112`;
+      case "eftpos":
+        return `#56B849`;
+      case "amex":
+        return `#56B0EC`;
+      case "visa/master":
+      case "visa":
+      case "master":
+        return `#016272`;
       default:
         return `#a5a5a5`;
     }
