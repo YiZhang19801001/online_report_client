6648ab93aec6a2da938d1f74f8b3b0f771e41ce5
yizhang199 <yizhang19801001@gmail.com>
2019-04-26 11:13:32 +1000

    done: login

--- src/app/auth/Login.jsx
+++ src/app/auth/Login.jsx
@@ -1,77 +1,46 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import axios from "axios";
-import { history } from "../shared";
+import { Form, history } from "../shared";
+
+const loginReducer = (success = false, action) => {
+  switch (action.type) {
+    case "login": // handle login
+      axios
+        .post(`http://localhost:8000/api/auth/login`, action.payload)
+        .then(res => {
+          localStorage.setItem(
+            "aupos_online_report_user",
+            JSON.stringify(res.data)
+          );
+          history.push(`${process.env.PUBLIC_URL}/daily`);
+        });
+      return true;
+    default:
+      // default/initial reducers
+      return success;
+  }
+};
+
 export default () => {
-  const labels = { app_name: "online report" }; //!fake data
-  const [formValues, setFormValues] = useState({
-    name: "",
-    password: "",
-    remember_me: false
-  });
-
-  const handleInputChange = e => {
-    setFormValues({ ...formValues, [e.target.name]: e.target.value });
-  };
-
-  const handleFormSubmit = e => {
-    e.preventDefault();
-    if (formValues.name === "" || formValues.password === "") {
-      return false;
+  const [success, dispatch] = useReducer(loginReducer, false);
+  const fields = [
+    {
+      name: "name",
+      type: "text",
+      placeholder: "enter username",
+      labelText: "username"
+    },
+    {
+      name: "password",
+      type: "password",
+      placeholder: "enter password",
+      labelText: "password"
     }
-    axios.post(`http://localhost:8000/api/auth/login`, formValues).then(res => {
-      localStorage.setItem(
-        "aupos_online_report_user",
-        JSON.stringify(res.data)
-      );
-      history.push(`${process.env.PUBLIC_URL}/daily`);
-    });
-  };
-  const handleChecked = e => {
-    setFormValues({ ...formValues, remember_me: e.target.checked });
-  };
+  ];
   return (
     <div className="component-login">
-      <div className="header">{labels.app_name}</div>
-      <form onSubmit={handleFormSubmit}>
-        <div className="form-field">
-          <label htmlFor="name" className="text-label">
-            username
-          </label>
-          <input
-            type="text"
-            placeholder="enter username"
-            value={formValues.name}
-            name="name"
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="form-field">
-          <label htmlFor="password" className="text-label">
-            password
-          </label>
-          <input
-            type="password"
-            placeholder="enter password"
-            value={formValues.password}
-            name="password"
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="form-field checkbox">
-          <input
-            name="remember_me"
-            type="checkbox"
-            checked={formValues.remember_me}
-            onChange={handleChecked}
-          />
-          <label htmlFor="remember_me" className="checkbox-label">
-            remember_me
-          </label>
-        </div>
-        <div className="button-container">
-          <button>confirm</button>
-        </div>
-      </form>
+      <div className="header">Packing Assistance</div>
+      <Form fields={fields} dispatch={dispatch} />
     </div>
   );
 };
--- src/app/shared/index.js
+++ src/app/shared/index.js
@@ -2,4 +2,6 @@ import { history } from "./history";
 import Header from "./Header";
 import Table from "./Table";
 import Loading from "./Loading";
-export { history, Header, Table, Loading };
+import Form from "./components/Form";
+
+export { history, Header, Table, Loading, Form };
