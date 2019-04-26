92535f33045ed39f69583d7f39572213cdc28d5b
yizhang199 <yizhang19801001@gmail.com>
2019-04-26 11:57:41 +1000

    done: f-loading-cover when change date/ rerender reports

--- src/_reducers/index.js
+++ src/_reducers/index.js
@@ -1,9 +1,10 @@
-import { dateStart, dateEnd } from "../app/shared/reducers";
+import { dateStart, dateEnd, showModal } from "../app/shared/reducers";
 import dateForDailyReport from "../app/summary/reducers";
 import { combineReducers } from "redux";
 
 export default combineReducers({
   dateEnd,
   dateStart,
-  dateForDailyReport
+  dateForDailyReport,
+  showModal
 });
