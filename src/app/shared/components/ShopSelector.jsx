291da8e2f89406ba4ed0ec742aacb4299d863070
yi zhang <yizhang19801001@outlook.com>
2019-04-28 10:34:43 +1000

    done: selector layout

--- src/app/shared/components/ShopSelector.jsx
+++ src/app/shared/components/ShopSelector.jsx
@@ -1,14 +1,20 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { uniqueId } from 'lodash';
 export default ({ shop }) => {
+  const shops = ['豆捞', '有米酸奶'];
+  const renderOptions = () => {
+    return shops.map((shopName, index) => {
+      return <option key={uniqueId('shopOption')} value={index}>
+        {shopName}
+      </option>
+    })
+  }
+  const [index, setIndex] = useState(0);
   return (
     <div className="shop-selector">
-      <div className="selector">
-        <span className="value">{shop}</span>
-        <span className="indicator">
-          <img src="/selector.svg" alt="" />
-        </span>
-      </div>
+      <select value={index} className="selector" onChange={(e) => { setIndex(e.target.value) }}>
+        {renderOptions()}
+      </select>
     </div>
-  );
+  )
 };
